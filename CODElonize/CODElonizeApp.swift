5a5f46b11e52594282b90b61b13cb9c72a8811dd
taz <arthaz.anthony@binus.ac.id>
2026-07-02 10:43:07 +0800

    Initial Commit

---
+++ CODElonize/CODElonizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CODElonizeApp.swift
+//  CODElonize
+//
+//  Created by Arthaz's MacBook on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CODElonizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
